ffc61f0a0afd314423eaae35ddbe846cda286d21
sathwikpatel <cs7785@srmist.edu.in>
2025-02-26 11:26:06 +0530

    app

--- Crop-Recommendation-system/Crop_Recommendation-main/app.py
+++ Crop-Recommendation-system/Crop_Recommendation-main/app.py
@@ -10,7 +10,7 @@ mx = pickle.load(open(r'C:\Users\acer\Downloads\Crop-Recommendation-system\Crop-
 # Crop dictionary mapping
 crop_dict = {
     1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut",
-    6: "Papaya", 7: "Orange", 8: "Apple", 9: "Muskmelon", 10: "Watermelon",
+    6: "Papaya", 7: "Starfruit", 8: "Apple", 9: "Muskmelon", 10: "Watermelon",
     11: "Grapes", 12: "Mango", 13: "Banana", 14: "Pomegranate", 15: "Lentil",
     16: "Blackgram", 17: "Mungbean", 18: "Mothbeans", 19: "Pigeonpeas",
     20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"
